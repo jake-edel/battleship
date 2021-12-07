42ee654f5064c0201f7b90bbaa3f579b15104441
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-07 12:31:00 -0700

    test: Cell obj returns coordinate

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,10 +2,14 @@ require './lib/cell'
 
 RSpec.describe Cell do
   before(:each) do
-   @cell = Cell.new
+   @cell = Cell.new('A0')
   end
 
   it 'exists' do
     expect(@cell).to be_instance_of Cell
+  end
+
+  it 'can return a coordinate' do
+      expect(@cell.coordinate).to eq('A0')
   end
 end
