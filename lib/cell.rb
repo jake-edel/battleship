58254d127bc4f69f6ec505cc1de30666a89bfd81
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-07 16:58:00 -0700

    #ship method returns either nil or ship variable

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,15 @@
 class Cell
   attr_reader :coordinate
-  
+
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = false
   end
+
+  def ship
+    return nil unless @ship
+
+    @ship
+  end
+
 end
