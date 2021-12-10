d92f6b1d47193608c96b686c6ca99bc5f31d19ae
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-09 20:38:00 -0700

    test: valid_coordinate checks that string is valid coordinate

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,10 +10,31 @@ RSpec.describe Board do
 
   describe 'valid_coordinate' do
     it 'returns true when given a valid coordinate' do
-      expect(@board.valid_coordinate('A1'))
+      expect(@board.valid_coordinate?('A1')).to be true
+      expect(@board.valid_coordinate?('B1')).to be true
+      expect(@board.valid_coordinate?('C1')).to be true
+      expect(@board.valid_coordinate?('D1')).to be true
+      expect(@board.valid_coordinate?('A4')).to be true
+      expect(@board.valid_coordinate?('B4')).to be true
+      expect(@board.valid_coordinate?('C4')).to be true
+      expect(@board.valid_coordinate?('D4')).to be true
+    end
+    it 'is case insensitive' do
+      expect(@board.valid_coordinate?('a1')).to be true
+      expect(@board.valid_coordinate?('b1')).to be true
+      expect(@board.valid_coordinate?('c1')).to be true
+      expect(@board.valid_coordinate?('d1')).to be true
     end
 
     it 'returns false when given an invalid coordinate' do
+      expect(@board.valid_coordinate?('a0')).to be false
+      expect(@board.valid_coordinate?('aa')).to be false
+      expect(@board.valid_coordinate?('a5')).to be false
+      expect(@board.valid_coordinate?('zz')).to be false
+      expect(@board.valid_coordinate?('12')).to be false
+      expect(@board.valid_coordinate?('ca')).to be false
+      expect(@board.valid_coordinate?('e1')).to be false
+      expect(@board.valid_coordinate?('d5')).to be false
     end
   end
 end
