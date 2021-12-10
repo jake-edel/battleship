fe7055cb91994305e6422b19bc89a5473b0ac90d
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-10 09:09:55 -0700

    test: beging testing coordinate validation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,3 @@
-require './lib/cell'
 require './lib/ship'
 require './lib/board'
 
@@ -35,6 +34,45 @@ RSpec.describe Board do
       expect(@board.valid_coordinate?('ca')).to be false
       expect(@board.valid_coordinate?('e1')).to be false
       expect(@board.valid_coordinate?('d5')).to be false
+    end
+  end
+
+  describe 'valid_placement' do
+    describe 'coordinates_valid' do
+      before(:each) do
+        # @board = Board.new
+        # Valid Coords
+        @a = %w[a1 a2 a3 a4]
+        @b = %w[d4 d3 d2 d1]
+        @c = %w[a1 b1 c1 d1]
+        @d = %w[a2 b2 c2 d2]
+        @e = %w[d4 c4 b4 a4]
+
+        # Invalid Coords
+        @f = %w[dx4 c4 b4 a1]
+        @g = %w[a1 b2 e3 d4]
+        @h = %w[d4 14 b4 a1]
+        @i = %w[a3 b3 c3 aa4]
+        @j = %w[d2 d3 d4 4a]
+      end
+
+      it 'returns true if array of coordinates are valid input and unoccupied' do
+        expect(@board.coordinates_valid?(@a)).to be true
+        expect(@board.coordinates_valid?(@b)).to be true
+        expect(@board.coordinates_valid?(@c)).to be true
+        expect(@board.coordinates_valid?(@d)).to be true
+        expect(@board.coordinates_valid?(@e)).to be true
+      end
+      it 'returns true if array of coordinates are valid input and unoccupied' do
+        expect(@board.coordinates_valid?(@f)).to be false
+        expect(@board.coordinates_valid?(@g)).to be false
+        expect(@board.coordinates_valid?(@h)).to be false
+        expect(@board.coordinates_valid?(@i)).to be false
+        expect(@board.coordinates_valid?(@j)).to be false
+      end
+    end
+    it 'returns true if ship can be placed on coordinates provided' do
+      expect(@board.valid_placement(Ship.new('Battleship', 4), %w[A1 A2 A3 A4])).to be true
     end
   end
 end
