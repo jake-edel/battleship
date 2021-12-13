6062450533b874417ecf350e5af51dc5531a4d9d
Alicia Watt <alicia.watt.aw@gmail.com>
2021-12-13 15:28:07 -0700

    create files

---
+++ lib/game_class.rb
@@ -0,0 +1,14 @@
+require './lib/computer_player'
+require './lib/human_player'
+
+class Game
+    def initialize
+    @player = HumanPlayer.new
+    @computer = ComputerPlayer.new
+    end
+
+def start
+  
+end
+
+end
