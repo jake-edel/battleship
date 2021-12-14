feb35d98052bde52f4daad4e19e7a79727e023ac
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-14 08:46:52 -0700

    test: Player initialized with empty ship array

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,10 +13,18 @@ RSpec.describe Player do
     it 'is initialized with an empty board' do
       expect(player.board).to be_instance_of Board
       expect(player.board.cells).to be_instance_of Hash
-      player.board.cells.each do |coordinate, cell|
+      player.board.cells.each do |_coordinate, cell|
         expect(cell).to be_instance_of Cell
         expect(cell.empty?).to be true
       end
     end
+
+    it 'is initialized with an empty ships array' do
+      expect(player.ships).to be_instance_of Array
+      expect(player.ships).to be_empty
+    end
+
+
+
   end
 end
