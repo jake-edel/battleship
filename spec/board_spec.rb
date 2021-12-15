d6813c3007aa8297b3f3c658ac574a3302f6a981
Alicia Watt <alicia.watt.aw@gmail.com>
2021-12-15 12:34:12 -0700

    test: prints board with ship

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -156,6 +156,11 @@ RSpec.describe Board do
      it 'returns a correct string' do
         expect(@board.render).to eq("  1 2 3 4 \nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
      end
+     it 'prints a board with ship' do
+       @board.place(@cruiser, ["A1", "A2", "A3"])
+       expect(@board.render(true)).to eq( "  1 2 3 4 \nA S S S .\nB . . . .\nC . . . .\nD . . . .\n")
+       expect(@board.render).to eq ( "  1 2 3 4 \nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
+     end
     end
   end
 end
